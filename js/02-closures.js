fba3bc4a2855d62a9a4b2ad0497a31f7641383be
InnaKharchenko <inna01kharchenko@gmail.com>
2022-10-30 22:01:47 +0200

    Update 02-closures.js

--- js/02-closures.js
+++ js/02-closures.js
@@ -42,3 +42,27 @@ const makeSheff = function (name) {
 
 // mango('котлети');
 // mango('пиріжок');
+
+
+// Окргулятор
+
+// const floatingPoint = 3.23456789;
+// const someIn = Math.round(floatingPoint);
+// const withDecimals = Number(floatingPoint.toFixed(2));
+
+// const rounder = function (number, places) {
+//     return Number(number.toFixed(places));
+// };
+
+const rounder = function (places) {
+    return function (number) {
+        return Number(number.toFixed(places));
+    }
+};
+
+const rounder2 = rounder(2);
+const rounder3 = rounder(3);
+
+console.dir(rounder2);
+console.dir(rounder3);
+
